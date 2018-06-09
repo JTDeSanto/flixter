cbf873bd3eada6fd3610f0a4dea87b6498141d13
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-06-09 15:07:07 -0700

    Add stripe payment and adjust layout

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+ before_action :require_authorized_user_for_lesson, only: [:show]
 
   def show  
   end
@@ -6,9 +8,19 @@ class LessonsController < ApplicationController
   private
 
 
+  def require_authorized_user_for_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      
+      redirect_to course_path(current_lesson.section.course), alert: "Please Enroll in Course to View Lesson"
+
+    end
+  end
+
   helper_method :current_lesson
     def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
     end
+
+  
 
 end
